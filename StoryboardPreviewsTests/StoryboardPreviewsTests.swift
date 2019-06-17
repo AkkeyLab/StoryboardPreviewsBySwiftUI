d6129350c73a5a775eb3a880b740d14c625ad41a
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2019-06-17 17:44:31 +0900

    :new: Create base project

---
+++ StoryboardPreviewsTests/StoryboardPreviewsTests.swift
@@ -0,0 +1,34 @@
+//
+//  StoryboardPreviewsTests.swift
+//  StoryboardPreviewsTests
+//
+//  Created by AKIO on 2019/06/17.
+//  Copyright © 2019 AKIO. All rights reserved.
+//
+
+import XCTest
+@testable import StoryboardPreviews
+
+class StoryboardPreviewsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
